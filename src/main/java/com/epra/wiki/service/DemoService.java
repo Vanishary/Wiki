ece471afd49d63897a66fcb3d99003ee5ed08c5d
Vanishary <505718579@qq.com>
2022-08-15 16:25:34 +0800

    生成demo代码

---
+++ src/main/java/com/epra/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.epra.wiki.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: Guotao Li
+ * @DateTime: 2022/8/14 10:31 下午
+ * @Description: TODO
+ */
+@Service
+public class DemoService {
+
+//    @Resource
+//    private DemooMapper demoMapper;
+//
+//    public List<Demo> list() {
+//        return demoMapper.list();
+//    }
+}
