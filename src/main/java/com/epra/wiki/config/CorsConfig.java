cf81e89334d7b0a13396d42920d11e74ca560c62
Vanishary <505718579@qq.com>
2022-08-17 18:35:34 +0800

    使用axios调用后端接口

---
+++ src/main/java/com/epra/wiki/config/CorsConfig.java
@@ -0,0 +1,9 @@
+package com.epra.wiki.config;
+
+/**
+ * @Author: Guotao Li
+ * @DateTime: 2022/8/17 6:28 下午
+ * @Description: TODO
+ */
+public class CorsConfig {
+}
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,8 +3,6 @@
         <a-layout-sider width="200" style="background: #fff">
             <a-menu
                     mode="inline"
-                    v-model:selectedKeys="selectedKeys2"
-                    v-model:openKeys="openKeys"
                     :style="{ height: '100%', borderRight: 0 }"
             >
                 <a-sub-menu key="sub1">
@@ -54,11 +52,15 @@
 <script lang="ts">
     import {defineComponent} from 'vue';
     import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import axios from 'axios';
 
     export default defineComponent({
         name: 'Home',
-        components: {
-            HelloWorld,
-        },
+        setup() {
+            console.log("setup")
+            axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+                console.log(response);
+            })
+        }
     });
 </script>
