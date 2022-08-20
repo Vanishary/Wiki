cb36cb93247a2f9f50112f138bb5dc6260d3b950
Vanishary <505718579@qq.com>
2022-08-20 13:34:27 +0800

    增加AOP，打印接口耗时、请求参数和返回参数，去掉过滤器、拦截器

--- src/main/java/com/epra/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/epra/wiki/config/SpringMvcConfig.java
@@ -1,25 +1,25 @@
-package com.epra.wiki.config;
-
-import com.epra.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-/**
- * @Author: Guotao Li
- * @DateTime: 2022/8/20 10:30 上午
- * @Description: TODO
- */
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.epra.wiki.config;
+//
+//import com.epra.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * @Author: Guotao Li
+// * @DateTime: 2022/8/20 10:30 上午
+// * @Description: TODO
+// */
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
--- src/main/java/com/epra/wiki/filter/LogFilter.java
+++ src/main/java/com/epra/wiki/filter/LogFilter.java
@@ -1,38 +1,38 @@
-package com.epra.wiki.filter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-/**
- * @Author: Guotao Li
- * @DateTime: 2022/8/18 10:30 下午
- * @Description: TODO
- */
-@Component
-public class LogFilter implements Filter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
-        // 打印请求信息
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        LOG.info("------------- LogFilter 开始 -------------");
-        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-        LOG.info("远程地址: {}", request.getRemoteAddr());
-
-        long startTime = System.currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-    }
-}
+//package com.epra.wiki.filter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+///**
+// * @Author: Guotao Li
+// * @DateTime: 2022/8/18 10:30 下午
+// * @Description: TODO
+// */
+//@Component
+//public class LogFilter implements Filter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+//        // 打印请求信息
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("------------- LogFilter 开始 -------------");
+//        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+//        LOG.info("远程地址: {}", request.getRemoteAddr());
+//
+//        long startTime = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//    }
+//}
