d17b089ce54ddd33c8b5869e33db21409c5696b8
Vanishary <505718579@qq.com>
2022-08-20 16:39:21 +0800

    电子书管理表格展示数据

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'vue/no-unused-components' : 'off',
         '@typescript-eslint/no-explicit-any' : 0,
-        'no-irregular-whitespace' : 'off'
+        'no-irregular-whitespace' : 'off',
+        'vue/no-unused-vars' : 'off'
     }
 }
