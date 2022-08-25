e100a41194dac3ea5e9eb0e2cc62bf0be68379f8
Vanishary <505718579@qq.com>
2022-08-25 18:33:27 +0800

    加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -292,10 +292,10 @@
                         console.log("树形结构：", level1.value);
 
                         // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
-                        // handleQuery({
-                        //     page: 1,
-                        //     size: pagination.value.pageSize,
-                        // });
+                        handleQuery({
+                            page: 1,
+                            size: pagination.value.pageSize,
+                        });
                     } else {
                         message.error(data.message);
                     }
@@ -353,10 +353,7 @@
 
             onMounted(() => {
                 handleQueryCategory();
-                handleQuery({
-                    page: 1,
-                    size: pagination.value.pageSize,
-                });
+
             });
 
             return {
