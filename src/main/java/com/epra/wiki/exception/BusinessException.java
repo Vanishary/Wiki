63114a17cd0a2036f6f9194b011df9aeef179bc6
Vanishary <505718579@qq.com>
2023-06-11 20:03:23 +0800

    使登录名在编辑时不可输入，新增时可输入

--- src/main/java/com/epra/wiki/exception/BusinessException.java
+++ src/main/java/com/epra/wiki/exception/BusinessException.java
@@ -5,5 +5,27 @@ package com.epra.wiki.exception;
  * @DateTime: 2023/6/11 7:36 下午
  * @Description: TODO
  */
-public class BusinessException {
+public class BusinessException extends RuntimeException{
+    private BusinessExceptionCode code;
+
+    public BusinessException(BusinessExceptionCode code) {
+        super(code.getDesc());
+        this.code = code;
+    }
+
+    public BusinessExceptionCode getCode() {
+        return code;
+    }
+
+    public void setCode(BusinessExceptionCode code) {
+        this.code = code;
+    }
+
+    /**
+     * 不写入堆栈信息，提高性能
+     */
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
 }
