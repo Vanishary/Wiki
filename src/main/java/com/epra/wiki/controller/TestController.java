64f8deb0efa808deeb35162982b639436a8ce873
Vanishary <505718579@qq.com>
2023-06-12 16:41:21 +0800

    增加redis测试接口

--- src/main/java/com/epra/wiki/controller/TestController.java
+++ src/main/java/com/epra/wiki/controller/TestController.java
@@ -5,12 +5,12 @@ import com.epra.wiki.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: Guotao Li
@@ -28,6 +28,9 @@ public class TestController {
     @Resource
     private TestService testService;
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @GetMapping("/hello")
     public String hello() {
         return "{\"name\":\"Hello World!!!\"}" + testName;
@@ -41,5 +44,19 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list() {
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
