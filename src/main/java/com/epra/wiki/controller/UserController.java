8f14cd55afc9c3dbde4f8b13ce6df56f0b8bb17d
Vanishary <505718579@qq.com>
2023-06-12 16:23:41 +0800

    集成redis,登录成功后将登录信息放入redis中

--- src/main/java/com/epra/wiki/controller/UserController.java
+++ src/main/java/com/epra/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.epra.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.epra.wiki.req.UserLoginReq;
 import com.epra.wiki.req.UserQueryReq;
 import com.epra.wiki.req.UserResetPasswordReq;
@@ -9,13 +10,16 @@ import com.epra.wiki.resp.PageResp;
 import com.epra.wiki.resp.UserLoginResp;
 import com.epra.wiki.resp.UserQueryResp;
 import com.epra.wiki.service.UserService;
+import com.epra.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: Guotao Li
@@ -30,6 +34,12 @@ public class UserController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq userQueryReq) {
@@ -67,6 +77,13 @@ public class UserController {
         userLoginReq.setPassword(DigestUtils.md5DigestAsHex(userLoginReq.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(userLoginReq);
+
+        // 生成单点登录token，并放入redis中
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token:{}，并放入redis中", token.toString());
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
