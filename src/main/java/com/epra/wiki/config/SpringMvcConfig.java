3f7c2163b81d2adbfddcce1dfe976e23b83a9096
Vanishary <505718579@qq.com>
2023-06-14 15:19:06 +0800

    前后端增加登录拦截

--- src/main/java/com/epra/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/epra/wiki/config/SpringMvcConfig.java
@@ -1,25 +1,34 @@
-//package com.epra.wiki.config;
-//
-//import com.epra.wiki.interceptor.LogInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-///**
-// * @Author: Guotao Li
-// * @DateTime: 2022/8/20 10:30 上午
-// * @Description: TODO
-// */
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+package com.epra.wiki.config;
+
+import com.epra.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Guotao Li
+ * @DateTime: 2022/8/20 10:30 上午
+ * @Description: TODO
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
+}
