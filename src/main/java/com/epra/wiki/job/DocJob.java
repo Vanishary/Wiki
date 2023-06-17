2ecb31bf49468d474e1aa5489e0dac6514164c62
Vanishary <505718579@qq.com>
2023-06-17 18:46:29 +0800

    增加日志流水号，方便生产运维

--- src/main/java/com/epra/wiki/job/DocJob.java
+++ src/main/java/com/epra/wiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.epra.wiki.job;
 
 import com.epra.wiki.service.DocService;
+import com.epra.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -21,11 +23,17 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 每30秒更新电子书信息
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+
         Long start = System.currentTimeMillis();
 //        LOG.info("更新电子书下文档数据开始，耗时：{} 毫秒", start);
         docService.updateEbookInfo();
