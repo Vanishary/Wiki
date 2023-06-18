8c80f4a6a4449f83d488230ac6f6d86cc8830255
Vanishary <505718579@qq.com>
2023-06-18 14:33:14 +0800

    将业务线程日志流水号传递到异步线程，方便生产运维

--- src/main/java/com/epra/wiki/service/WsService.java
+++ src/main/java/com/epra/wiki/service/WsService.java
@@ -1,6 +1,7 @@
 package com.epra.wiki.service;
 
 import com.epra.wiki.WebSocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ public class WsService {
     public WebSocketServer webSocketServer;
 
     @Async
-    public void sendInfo(String message) {
+    public void sendInfo(String message, String logId) {
+        MDC.put("LOG_ID", logId);
         webSocketServer.sendInfo(message);
     }
 }
